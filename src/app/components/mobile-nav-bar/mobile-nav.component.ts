92c7866240c06d472d7fd781aaf6be322c0c3918
Kam <sirkamronwilson00@gmail.com>
2025-04-07 07:20:53 -0500

    making auth screens resposive

--- src/app/components/mobile-nav-bar/mobile-nav.component.ts
+++ src/app/components/mobile-nav-bar/mobile-nav.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth/auth-service';
 
 @Component({
   selector: 'mobile-nav-menu',
@@ -9,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './mobile-nav.component.css',
   imports: [MatSidenavModule, MatButtonModule, RouterOutlet],
 })
-export class MobileNav {}
+export class MobileNav {
+  readonly authService = inject(AuthService);
+  readonly router = inject(Router);
+
+  onLogoutClick(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
